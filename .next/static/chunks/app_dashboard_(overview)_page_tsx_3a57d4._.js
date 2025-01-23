61b4c67b43a89e9947f1c5df029edbc7ffce8ff5
CumMblLLl <gladiaht@gmail.com>
2025-01-23 17:08:16 +0200

    completed chapter 10

---
+++ .next/static/chunks/app_dashboard_(overview)_page_tsx_3a57d4._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_dashboard_(overview)_page_tsx_3a57d4._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_dashboard_(overview)_page_tsx_3a57d4._.js",
+  "chunks": [
+    "static/chunks/b7e6c_next_dist_617f91._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/app_layout_tsx_dbb5f2._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_dbb5f2._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_dbb5f2._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__81e440._.css",
+    "static/chunks/b7e6c_next_dist_98dbba._.js"
+  ],
+  "source": "dynamic"
+});
