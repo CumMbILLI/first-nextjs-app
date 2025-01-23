b7fba29f05a72a6de8f4b98ab9bb8dfcc7a149ad
CumMblLLl <gladiaht@gmail.com>
2025-01-23 16:39:24 +0200

    completed chapter 9

--- app/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { inter } from "../ui/fonts";
-
-export default function Loading() {
-  return (
-    <p className={`${inter.className} text-center text-xl`}>
-      <b>Loading...</b>
-    </p>
-  );
-}
